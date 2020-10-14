2b2380c9131d61696dacd39de39c1768a3bd7b4b
trineva <alisa.trineva@ya.ru>
2020-10-14 10:29:03 +1000

    2е занятие

--- main.py
+++ main.py
@@ -7,11 +7,41 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0)
+numeral = ''
+move = 1
+block = 0
+start = 1
+OUTSIDE_BG = (0, -100)
 
 pygame.init()
 pygame.display.set_caption("угадай число ")
 screen = pygame.display.set_mode((W, H))
-pygame.mouse.set_visible(False)
+pygame.mouse.set_visible(True)
+
+font = pygame.font.SysFont('Arial', 28, True, False)
+font_box = pygame.Surface((W - font.get_height(), font.get_height()))
+font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
+font = pygame.font.SysFont('Arial', 14, True, False)
+
+
+bg = pygame. image.load('image/room.png')
+bg_rect = bg.get_rect(topleft=(0, 0))
+
+cat = pygame. image.load('image/cat.png')
+cat_rect = cat.get_rect(center=(70, 220))
+
+dog = pygame. image.load('image/dog.png')
+dog_rect = dog.get_rect(center=(410, 220))
+
+owl = pygame. image.load('image/owl.png')
+owl_rect = owl.get_rect(center=(210, 120))
+
+dialog = pygame. image.load('image/dialog.png')
+dialog_rect = dialog.get_rect()
+dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+
+
 
 run = True
 while run:
@@ -21,3 +51,9 @@ while run:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_ESCAPE:
                 run = False
+
+    screen.blit(bg, bg_rect)
+    screen.blit(cat, cat_rect)
+    screen.blit(dog, dog_rect)
+    screen.blit(owl, owl_rect)
+    pygame.display.update()
---
+++ test..py
@@ -0,0 +1,12 @@
+'''
+def f(a, b):
+    c = a + b
+    print(c)
+
+
+f(5, 10, 2)
+'''
+
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((480, 360))
