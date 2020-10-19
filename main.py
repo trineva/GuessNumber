d0bdf0f09cbd4b7488e4343ffbbb29ef368b0173
trineva <alisa.trineva@ya.ru>
2020-10-19 10:23:33 +1000

    -

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ pygame.mouse.set_visible(True)
 font = pygame.font.SysFont('Arial', 28, True, False)
 font_box = pygame.Surface((W - font.get_height(), font.get_height()))
 font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
-font = pygame.font.SysFont('Arial', 14, True, False)
+font2 = pygame.font.SysFont('Arial', 14, True, False)
 
 
 bg = pygame. image.load('image/room.png')
@@ -29,6 +29,7 @@ bg_rect = bg.get_rect(topleft=(0, 0))
 
 cat = pygame. image.load('image/cat.png')
 cat_rect = cat.get_rect(center=(70, 220))
+print(cat_rect)
 
 dog = pygame. image.load('image/dog.png')
 dog_rect = dog.get_rect(center=(410, 220))
@@ -40,7 +41,16 @@ dialog = pygame. image.load('image/dialog.png')
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
 dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
 
+
+def dialogs(text, pos, owl_text):
+    screen.blit(dialog, pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    screen.blit(dialog, dialog_owl_pos)
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
 
 
 run = True
@@ -51,9 +61,21 @@ while run:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_ESCAPE:
                 run = False
+            elif e.unicode.isdecimal() and block == 0:
+                numeral += e.unicode
 
-    screen.blit(bg, bg_rect)
-    screen.blit(cat, cat_rect)
-    screen.blit(dog, dog_rect)
-    screen.blit(owl, owl_rect)
+    if start == 0:
+        screen.blit(bg, bg_rect)
+        screen.blit(cat, cat_rect)
+        screen.blit(dog, dog_rect)
+        screen.blit(owl, owl_rect)
+        screen.blit(font_box, font_rect)
+        font_box.fill(SILVER)
+        font_box.blit(font.render(numeral, True, BLACK), (10, 0))
     pygame.display.update()
+
+    if start == 1:
+        dialogs('', OUTSIDE_BG, 'я загадала число')
+        dialogs('', OUTSIDE_BG, 'от нуля до 100')
+        dialogs('кот, твой ход', dialog_dog_pos, 'отгадайте его')
+        start = 0
